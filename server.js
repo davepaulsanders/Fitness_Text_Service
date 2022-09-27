94f028643fe05de4b040dc5281737b86ff83e6f4
Dave Sanders <davepaulsanders@gmail.com>
2022-09-27 10:54:42 -0400

    initial project set up and twilio messaging configured

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+19123257761',
+     to: '+16095294847'
+   })
+  .then(message => console.log(message.sid));
