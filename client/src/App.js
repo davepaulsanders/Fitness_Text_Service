6bb043b4d72ac252ea54f689b1d4986d7aaccd5b
Dave Sanders <davepaulsanders@gmail.com>
2022-10-06 12:57:00 -0400

    Adding combobox from headlessui to allow for searching and selecting multiple clients to send text

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import { Login } from "./components/Login/Login";
+import { ComboBox } from "./components/ComboBox/ComboBox";
 
 function App() {
   return (
     <div className="App flex justify-center items-center w-full">
-    
+      <ComboBox></ComboBox>
     </div>
   );
 }
