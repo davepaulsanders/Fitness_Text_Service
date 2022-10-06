e9fd2c10e635bac7e5d2528c31e94fb1ddb92ba9
Dave Sanders <davepaulsanders@gmail.com>
2022-10-06 12:16:23 -0400

    Completing log in design for now:

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Login } from "./components/Login/Login";
 function App() {
   return (
     <div className="App flex justify-center items-center w-full">
-      <Login />
+    
     </div>
   );
 }
