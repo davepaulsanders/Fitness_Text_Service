9f6afffd3ca7872e848573139ae289ae41d22a3c
Dave Sanders <davepaulsanders@gmail.com>
2022-10-07 16:03:59 -0400

    Setting up react router

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,25 @@ import { useState } from "react";
 import { Header } from "./components/Header/Header";
 import { Login } from "./components/Login/Login";
 import { Landing } from "./pages/Landing/Landing";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   const [loggedIn, setLoggedIn] = useState(true);
   if (loggedIn) {
     return (
       <div className="App flex flex-col justify-center items-center">
-        <Header /> <Landing />
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Header />
+                  <Landing />
+                </>
+              }
+            />
+          </Routes>
+        </Router>
       </div>
     );
   } else {
--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 export const Landing = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      <div className="flex flex-col shadow rounded-md justify-center items-center p-6">
+      <div className="flex flex-col shadow rounded-md justify-center items-center p-6 bg-white">
         <h2 className="mb-12 text-4xl">Welcome Bonnie and Will!</h2>
-        <button className="bg-blue-400 hover:bg-blue-500 text-xl w-3/4 mb-1 py-2 px-3 rounded-md">Send one-time messages</button>
-        <button className="bg-blue-400 hover:bg-blue-500 text-xl w-3/4 mb-1 py-2 rounded-md">Edit daily messages</button>
-        <button className="bg-blue-400 hover:bg-blue-500 text-xl w-3/4 py-2 rounded-md">Edit clients</button>
+        <button className="bg-blue-400 hover:bg-blue-500 text-xl w-3/4 mb-3 py-2 px-3 rounded-md">
+          Send one-time messages
+        </button>
+        <button className="bg-blue-400 hover:bg-blue-500 text-xl w-3/4 mb-3 py-2 rounded-md">
+          Edit daily messages
+        </button>
+        <button className="bg-blue-400 hover:bg-blue-500 text-xl w-3/4 py-2 rounded-md">
+          Edit clients
+        </button>
       </div>
     </div>
   );
