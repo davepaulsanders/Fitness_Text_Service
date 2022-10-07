982bd32ea3bcaabc9e03b22406ad57eb0c55fa64
Dave Sanders <davepaulsanders@gmail.com>
2022-10-07 15:10:51 -0400

    .

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,12 @@ import "./App.css";
 import { Header } from "./components/Header/Header";
 
 function App() {
-  return <div className="App flex flex-col justify-center items-center">
-    <Header />
-  </div>;
+  return (
+    <div className="App flex flex-col justify-center items-center">
+      <Header />
+      
+    </div>
+  );
 }
 
 export default App;
