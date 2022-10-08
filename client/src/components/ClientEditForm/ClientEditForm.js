c632237cb5d54f62ec56f948efdf499748d2600e
Dave Sanders <davepaulsanders@gmail.com>
2022-10-08 15:13:23 -0400

    Changing responsiveness of form

--- client/src/components/ClientEditForm/ClientEditForm.js
+++ client/src/components/ClientEditForm/ClientEditForm.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export const ClientEditForm = ({ client }) => {
   return (
-    <div className="rounded-md p-6 shadow w-11/12 md:w-7/12 lg:w-5/12 xl:w-4/12 mt-5 md:mt-0">
-      <div className="flex flex-col md:flex-row justify-around">
+    <div className="rounded-md shadow p-4 w-full max-w-md mt-5 md:mt-0">
+      <div className="flex flex-col md:flex-row justify-between w-full">
         <div className="flex flex-col mb-2">
           <label className="text-left mb-1">First Name</label>
           <input
-            className="form-input border border-black rounded-md py-2 shadow-inner"
+            className="form-input border border-black rounded-md py-2 shadow-inner mr-1"
             type="text"
             value={client.firstName}
           />
@@ -15,17 +15,17 @@ export const ClientEditForm = ({ client }) => {
         <div className="flex flex-col mb-5">
           <label className="text-left mb-1">Last Name</label>
           <input
-            className="form-input border border-black rounded-md py-2 shadow-inner"
+            className="form-input border border-black rounded-md py-2 shadow-inner ml-1"
             type="text"
             value={client.lastName}
           />
         </div>
       </div>
-      <div className="flex flex-col md:flex-row justify-around">
+      <div className="flex flex-col md:flex-row justify-between">
         <div className="flex flex-col mb-2">
           <label className="text-left mb-1">Email</label>
           <input
-            className="form-input border border-black rounded-md py-2 shadow-inner truncate"
+            className="form-input border border-black rounded-md py-2 shadow-inner truncate mr-1"
             type="text"
             value={client.email}
           />
@@ -33,7 +33,7 @@ export const ClientEditForm = ({ client }) => {
         <div className="flex flex-col mb-5">
           <label className="text-left mb-1">Phone</label>
           <input
-            className="form-input border border-black rounded-md py-2 shadow-inner"
+            className="form-input border border-black rounded-md py-2 shadow-inne ml-1"
             type="text"
             value={client.phoneNumber}
           />
@@ -42,16 +42,16 @@ export const ClientEditForm = ({ client }) => {
       <div className="flex flex-col justify-center items-center mb-2">
         <label className="text-left mb-1">Weight Loss Goals</label>
         <textarea
-          className="form-input border border-black rounded-md py-2 shadow-inner w-full"
+          className="form-input border border-black rounded-md py-2 shadow-inner w-full md:w-"
           type="text"
           value={client.weightLossGoals}
         />
       </div>
-      <div className="flex flex-col md:flex-row justify-around">
+      <div className="flex flex-col md:flex-row justify-between">
         <div className="flex flex-col mb-5">
           <label className="text-left mb-1">Days Elapsed</label>
           <input
-            className="form-input border border-black rounded-md py-2 shadow-inner"
+            className="form-input border border-black rounded-md py-2 shadow-inner mr-1"
             type="text"
             value={client.daysElapsed}
           />
@@ -59,7 +59,7 @@ export const ClientEditForm = ({ client }) => {
         <div className="flex flex-col mb-5">
           <label className="text-left mb-1">Spend Total</label>
           <input
-            className="form-input border border-black rounded-md py-2 shadow-inner"
+            className="form-input border border-black rounded-md py-2 shadow-inner ml-1"
             type="text"
             value={client.spendTotal}
           />
