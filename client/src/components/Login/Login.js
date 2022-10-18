64c237992eb1dabb91ff9ff5f0753c413bd7a290
Dave Sanders <davepaulsanders@gmail.com>
2022-10-18 09:49:28 -0400

    Login route created that sends back token, deleted create route since it will not be needed

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    const body = { username, password };
+
+    e.preventDefault();
+    const token = await fetch("http://localhost:3001/api/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+    if (token.error) {
+    } else {
+      localStorage.setItem("jwt", token.userToken);
+    }
+  };
   return (
-    <div className="p-6 w-11/12 lg:w-4/12 shadow bg-white">
+    <form
+      className="p-6 w-11/12 lg:w-4/12 shadow bg-white"
+      onSubmit={handleSubmit}
+    >
       <h1 className="text-left mb-6 text-2xl font-bold">
         Welcome Bonnie and Will!
       </h1>
@@ -14,6 +34,7 @@ export const Login = () => {
           id="username"
           className="form-input py-2 shadow-inner"
           type="text"
+          onChange={(e) => setUsername(e.target.value)}
         />
       </div>
       <div className="flex flex-col mb-5">
@@ -24,11 +45,15 @@ export const Login = () => {
           id="password"
           className="form-input py-2 shadow-inner"
           type="password"
+          onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      <button className="bg-blue-400 hover:bg-blue-500 text-xl py-2 w-full rounded-md">
+      <button
+        type="submit"
+        className="bg-blue-400 hover:bg-blue-500 text-xl py-2 w-full rounded-md"
+      >
         Login
       </button>
-    </div>
+    </form>
   );
 };
--- server/controllers/apiRoutes/userRoutes.js
+++ server/controllers/apiRoutes/userRoutes.js
@@ -3,15 +3,27 @@ const User = require("../../models/User");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-router.post("/", async (req, res) => {
-  const user = await User.create(req.body);
-  const payload = {
-    user_id: user._id,
-    username: user.username
-  }
-  // create token and send back
-  const token = jwt.sign(payload, process.env.JWT_SECRET);
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ username: req.body.username });
 
-  res.send({ userToken: `Bearer ${token}` });
+  if (user) {
+    // check password, then send back token
+    const validPassword = await user.comparePassword(req.body.password);
+
+    if (validPassword) {
+      const payload = {
+        user_id: user._id,
+        username: user.username,
+      };
+      // create token and send back
+      const token = jwt.sign(payload, process.env.JWT_SECRET);
+
+      res.send({ userToken: `Bearer ${token}` });
+    } else {
+      res.json({ error: "Your username/password is incorrect" });
+    }
+  } else {
+    res.json({ error: "Your username/password is incorrect" });
+  }
 });
 module.exports = router;
